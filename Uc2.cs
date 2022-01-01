4cff4050ea5ad2f8a84f315639513b8b0ce41da4
shaikmohammed238 <shaikmohammedghoue238@gmail.com>
2022-01-01 23:58:05 +0530

    added uc2

---
+++ Uc2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace DAY__19_USER_REGISTRATION_PROBLEM
+{ 
+    //UC2  Last name starts with Cap and has minimum 3 characters
+    class Uc2
+    {
+      public static string REGEX_LASTNAME = "^[A-Z]{1}[A-Z a-z]{2,}$";
+        public bool ValidateName(string LASTNAME)
+        {
+          return Regex.IsMatch(LASTNAME, REGEX_LASTNAME);
+        }
+    }
+}
