9cde7ec505b012946f4a6f3cbac7f2de59980ee8
shaikmohammed238 <shaikmohammedghoue238@gmail.com>
2022-01-02 09:34:46 +0530

    added uc9

---
+++ Uc9.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace DAY__19_USER_REGISTRATION_PROBLEM
+{
+    //Uc9 Should clear all email samples provided separately {check all emails}
+    public class Uc9
+    {
+      public static string REGEX_VALIDEMAILS = @"^[0-9A-Za-z]+([._+-][0-9A-Za-z]+)*[@][0-9A-Za-z]+.[a-zA-Z]{2,3}(.[a-zA-Z]{2,3})?$";
+       public bool ValidateEmails(string Emails)
+       {
+         return Regex.IsMatch(Emails, REGEX_VALIDEMAILS);
+       }
+    }
+}
