e0c17d04cfb6f82b7d73c91ce245985c1f8e339c
shaikmohammed238 <shaikmohammedghoue238@gmail.com>
2022-01-02 09:14:57 +0530

    added uc5

---
+++ Uc5.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace DAY__19_USER_REGISTRATION_PROBLEM
+{
+    //Uc5 As a User need to follow pre-defined Password rules.Rule1– minimum 8 Characters - -------{NOTE – All rules must be passed}
+    class Uc5
+    {
+      public static string REGEX_VALIDPASSWORD = "[a-z A-Z]{8,}";
+       public bool ValidatePassWord(string Password)
+       {
+         return Regex.IsMatch(Password, REGEX_VALIDPASSWORD);
+       }
+    }
+}
